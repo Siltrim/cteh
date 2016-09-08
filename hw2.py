62d575c860f0cc7da20fcd3c295f61c6a71a4b17
Gia <psyh.psyh@gmail.com>
2016-09-08 16:11:13 +0300

    homework1 with input and output files

---
+++ hw2.py
@@ -0,0 +1,28 @@
+#homework1.2
+import sys
+
+__author__ = 'gia_sebua'
+
+if sys.version_info[0] == 2:
+    input_function = raw_input
+else:
+    input_function = input
+
+separator = ';' #separator between question and answer in file with questions
+correct = 0
+questions = open('questions.txt','r')
+results = open('results.txt','a')
+name = input_function('Hi, what\'s your name? ')
+
+for line in questions:
+    line = line.split(separator)
+    answer = input_function(line[0].strip())
+    if answer == line[1].strip():
+        correct += 1
+        print('hell yeah! Correct answers:', correct)
+    else:
+        print('wrong answer')
+
+print('{} you have {} correct answers'.format(name, correct))
+output = name + ' ' + str(correct)+'\r\n'
+results.write(output)
