37d73116e70724e9f5b9ef62a383409c76bc9ad2
Gia <psyh.psyh@gmail.com>
2016-09-14 15:18:30 +0300

    lesson3. hw4. start

---
+++ homework3/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+#LF
+__author__ = 'gia_sebua'
---
+++ homework3/custom_exceptions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'gia_sebua'
+
+
+class UserExitException(KeyboardInterrupt):
+    pass
